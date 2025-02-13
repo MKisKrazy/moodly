2181795288e85a32786865c58cf7a43aba8fb279
mahihaasan <mahihaasan123@gmail.com>
2025-02-14 02:04:51 +0530

    all files added

---
+++ components/Login.jsx
@@ -0,0 +1,58 @@
+'use client'
+import React, { useState } from 'react'
+import Button from './Button'
+import { Fugaz_One } from 'next/font/google'
+import { useAuth } from '@/context/AuthContext'
+
+const fugaz= Fugaz_One({
+  subsets: ["latin"],
+  weight:['400']
+})
+
+export default function Login() {
+  const [email,setEmail]= useState('')
+  const [password,setPassword]=useState('')
+  const[isRegister,setIsRegister]=useState(false)
+  const [authenticating,setAuthenticating]=useState(false)
+  const { signup,login }= useAuth()
+
+  async function handleSubmit(){
+    if(!email || !password || password.length<6){
+      return
+    }
+    setAuthenticating(true)
+    try{
+    if (isRegister){
+      console.log('Signing up a new user')
+      await signup(email,password)
+    }else{
+      console.log("Logging in existing user")
+      await login(email,password)
+    }}catch(err){
+      console.log(err.message)
+    }finally{
+      setAuthenticating(false)
+    }
+  }
+
+
+  return (
+    <div className='flex flex-col flex-1 items-center justify-center gap-4 '>
+        <h3 className={' text-4xl sm:text-5xl md:text-6xl ' + fugaz.className }>{isRegister ? 'Register' : 'Log In'}</h3>
+        <p>You&apos;re one step away!</p>
+        <input value={email} onChange={(e)=>{
+          setEmail(e.target.value)
+        }} className='max-w-[400px] w-full mx-auto px-3 py-2 sm:py-3 rounded-full border border-solid outline-none border-indigo-400 duration-200 hover:border-indigo-600 focus:border-indigo-600 transition-all'placeholder='Email' type='email' />
+        <input value={password} onChange={(e)=>{
+          setPassword(e.target.value)
+        }} className='max-w-[400px] w-full mx-auto px-3 py-2 sm:py-3 rounded-full border  border-solid outline-none border-indigo-400 duration-200 hover:border-indigo-600 focus:border-indigo-600 transition-all' placeholder='Password' type='password'/>
+        <div className='max-w-[400px] w-full mx-auto'>
+          <Button clickHandler={handleSubmit} text={authenticating?'Submitting':'Submit'} full />
+        </div>
+        <p className='text-center'>{(isRegister? 'Already have an account?':"Don't have an account?")}<button onClick={()=>{
+          setIsRegister(!isRegister)
+        }} className='text-indigo-600 px-2'>{(isRegister?'Sign Up':'Sign In')}</button></p>
+        
+    </div>
+  )
+}
