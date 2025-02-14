39b4659b4b36a8e7886c93e0beb7eec04448587f
mahihaasan <mahihaasan123@gmail.com>
2025-02-15 00:30:09 +0530

    fixed button

--- components/Button.jsx
+++ components/Button.jsx
@@ -9,7 +9,7 @@ const fugaz= Fugaz_One({
 export default function Button(props) {
     const {text,dark,full,clickHandler} = props
   return (
-    <button onClick={clickHandler} className={' rounded-full duration-200 hover:opacity-60 transition-opacity border-2 border-solid border-indigo-600 overflow-hidden ' + (dark ? 'text-white bg-indigo-600' : 'text-indigo-600')
+    <button onClick={clickHandler} className={' rounded-full duration-200 hover:opacity-60 transition-opacity border-2 border-solid border-indigo-600 overflow-hidden text-indigo-600 ' + ( dark ? ' text-white bg-indigo-600 ' : ' ')
       + (full ? 'grid place-items-center w-full ' : ' ')
     }>
         <p className={' px-6 sm:px-10 whitespace-nowrap py-2 sm:py-3 ' + fugaz.className }>
